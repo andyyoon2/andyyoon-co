2d1a630223da766d89b1aa9b26433611a95ebdce
Andy Yoon <ayoon212@gmail.com>
2020-09-19 16:15:43 -0700

    Add initial pages

---
+++ pages/blog.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const Blog = () => (
+  <div>
+    <h1>Blog</h1>
+    <Link href="/">Home</Link>
+  </div>
+);
+
+export default Blog;
---
+++ pages/resume.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const Resume = () => (
+  <div>
+    <h1>Resume</h1>
+    <Link href="/">Home</Link>
+  </div>
+);
+
+export default Resume;
