ed7ca877102c89430538bd92a040e3ee6909d053
Andy Yoon <ayoon212@gmail.com>
2020-09-20 10:03:13 -0700

    Remove dev font, add new fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import '../styles/global.css';
 import theme from '../styles/theme';
@@ -8,13 +9,19 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
   }
   body {
-    font-family: 'Sinkin Sans', 'Helvetica', 'Arial', sans-serif;
+    font-family: 'Inter', 'Helvetica', 'Arial', sans-serif;
+  }
+  h1, h2, h3, h4, h5 {
+    font-family: 'Questrial', 'Helvetica', 'Arial', sans-serif;
   }
 `;
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet"/>
+      </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
