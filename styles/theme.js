93f312137ec0aadf02d2f63bc432547d779c1824
Andy Yoon <ayoon212@gmail.com>
2020-09-19 22:41:57 -0700

    Add global style and theme provider through styled-components

---
+++ styles/theme.js
@@ -0,0 +1,11 @@
+const moderateBlue = '#5786b9';
+const moderateRed = '#b95957';
+const moderateYellow = '#b7b957';
+
+const theme = {
+  primary: moderateBlue,
+  secondary: moderateYellow,
+  tertiary: moderateRed
+};
+
+export default theme;
