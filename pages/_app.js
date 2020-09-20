32450775f49f4f5a25f3120e1dc13e4f6e8f77fa
Andy Yoon <ayoon212@gmail.com>
2020-09-20 10:13:04 -0700

    Add text highlight color

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,9 @@ const GlobalStyle = createGlobalStyle`
   h1, h2, h3, h4, h5 {
     font-family: 'Questrial', 'Helvetica', 'Arial', sans-serif;
   }
+  ::selection {
+    background: ${({theme}) => theme.tertiary};
+  }
 `;
 
 export default function MyApp({ Component, pageProps }) {
@@ -22,8 +25,8 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Questrial&display=swap" rel="stylesheet"/>
       </Head>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
