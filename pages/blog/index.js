943c5e191e6d79c528774b07ce0c5f4f66e0f431
Andy Yoon <ayoon212@gmail.com>
2020-09-20 10:08:06 -0700

    Add blog link to article

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -7,7 +7,7 @@ const Blog = ({ posts }) => (
     <h1>Blog</h1>
     <ul>
       {posts.map(post => (
-        <li key={post.slug}>{post.title}</li>
+        <li key={post.slug}><Link href={`/blog/${post.slug}`}><a>{post.title}</a></Link></li>
       ))}
     </ul>
   </Layout>
