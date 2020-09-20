c89dd3018f0a781a5eeed66e03569de5a0a630d8
Andy Yoon <ayoon212@gmail.com>
2020-09-19 22:42:04 -0700

    Start styling sitemap links

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Layout from '../components/layout';
 import styled from 'styled-components';
+import Layout from '../components/layout';
+
+const shapeSpacing = '1.5rem';
+const shapeSize = '50px';
 
 const Main = styled.main`
   img {
@@ -10,35 +13,52 @@ const Main = styled.main`
     top: 0;
     z-index: -1;
   }
-  .spacer {
-    height: 180px;
-  }
   .content {
     background-color: #fff;
     color: #000;
     position: relative;
     text-align: center;
     h1, h2, h3 {
-      font-weight: 300;
+      // font-weight: 300;
     }
-    &.top h3 {
-      margin-bottom: 0;
-      padding-bottom: 1.5rem;
+    &.top {
+      padding: ${shapeSpacing} 0;
+      h1 {
+        margin-top: 0;
+      }
+      h3 {
+        margin-bottom: 0;
+      }
     }
   }
   .content-shape {
     position: absolute;
     &.trailing {
-      border-top: 50px solid #fff;
+      border-top: ${shapeSize} solid #fff;
       border-left: 100vw solid transparent;
       bottom: 0;
       transform: translateY(100%);
     }
     &.leading {
-      border-bottom: 50px solid #fff;
+      border-bottom: ${shapeSize} solid #fff;
       border-right: 100vw solid transparent;
       top: 0;
       transform: translateY(-100%);
+    }
+  }
+  .site-map {
+    padding-top: ${shapeSpacing};
+    a {
+      display: inline-block;
+      color: #000;
+      font-size: 2.75rem;
+      margin-bottom: 1em;
+      text-decoration-color: ${({theme}) => theme.primary};
+      text-decoration-thickness: 4px;
+      transition: all 0.2s ease-in-out;
+      :hover {
+        text-decoration-color: ${({theme}) => theme.secondary};
+      }
     }
   }
 `;
@@ -62,9 +82,11 @@ const Home = () => (
 
       <div className="content">
         <div className="content-shape leading"></div>
-        <Link href="resume"><a>Resume</a></Link>
-        <Link href="portfolio"><a>Portfolio</a></Link>
-        <Link href="blog"><a>Blog</a></Link>
+        <div className="site-map">
+          <div><Link href="resume"><a>Resume</a></Link></div>
+          <div><Link href="portfolio"><a>Portfolio</a></Link></div>
+          <div><Link href="blog"><a>Blog</a></Link></div>
+        </div>
 
         <h2>Contact n stuff</h2>
         <div>
