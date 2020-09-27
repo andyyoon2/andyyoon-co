2e73d969ffd71d5ca9b64231fb6c5b9113596804
Andy Yoon <ayoon212@gmail.com>
2020-09-27 16:47:30 -0700

    Restructure styled components for better readability

--- pages/index.js
+++ pages/index.js
@@ -12,27 +12,43 @@ const Main = styled.main`
   max-width: 1200px;
   margin-left: auto;
   position: relative;
-  
-  .top-content {
-    padding: 8rem 3rem;
-    text-align: left;
-    .name {
-      font-size: 3rem;
-      font-variation-settings: 'wght' 600;
-      margin: 0;
-    }
-    .greeting {
-      margin-top: 1.5rem;
-      margin-bottom: 0.5rem;
-    }
-    img.headshot {
-      width: 180px;
-      margin: 0 auto;
-      // slight correction for background lines in the image
-      transform: rotate(-2deg);
-    }
+  section {
+    margin: 10rem 3rem;
   }
-  
+`;
+
+const Hero = styled.section`
+  text-align: left;
+  .name {
+    font-size: 3rem;
+    font-variation-settings: 'wght' 600;
+    margin: 0;
+  }
+  .greeting {
+    margin-top: 1.5rem;
+    margin-bottom: 0.5rem;
+  }
+  img.headshot {
+    width: 180px;
+    margin: 0 auto;
+    // slight correction for background lines in the image
+    transform: rotate(-2deg);
+  }
+`;
+
+const Blurb = styled.section`
+  p {
+    line-height: 1.5;
+  }
+  a {
+    color: inherit;
+    font-variation-settings: 'wght' 525;
+    background-color: #42c9ff;
+  }
+`;
+
+
+const oldStyles = `
   .content {
     // background-color: #fff;
     position: relative;
@@ -72,17 +88,17 @@ const Home = () => (
     </Head>
 
     <Main>
-      <div className="top-content">
+      <Hero>
         <img className="headshot" src="/images/andy-cropped.jpg" alt="Andy Yoon" />
         <p className="greeting">Welcome! My name is</p>
         <Heading tag="h1" className="name">Andy Yoon</Heading>
-      </div>
-      <div>
+      </Hero>
+      <Blurb>
         <p>I'm a <Link href="resume">web developer</Link> and <Link href="portfolio">musician</Link> who lives to bring joy to others' lives and to influence people to be their best.</p>
         <p>My mission is to work together with passionate people to make a tangible, positive contribution to the world.</p>
         <p>Check out my work here</p>
         <p>[contact section] I'm always open to discuss collabs on new projects!</p>
-      </div>
+      </Blurb>
 
       <div className="content">
         <div className="site-map">
