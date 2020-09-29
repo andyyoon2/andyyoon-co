4052f3d663b077ade87b518ec11bbcd0cdac0787
Andy Yoon <ayoon212@gmail.com>
2020-09-29 10:18:18 -0700

    Convert colors to HSL, remove unused colors

--- styles/theme.js
+++ styles/theme.js
@@ -1,22 +1,17 @@
-const moderateBlue = '#5786b9';
-const moderateRed = '#b95957';
-const moderateYellow = '#b7b957';
-
-const lightBlue = '#42c9ff';
-const darkBlue = '#0075a4';
-const lightYellow = '#ffd742';
+const lightBlue = 'hsl(197, 100%, 63%)';
+const darkBlue = 'hsl(197, 100%, 32%)';
+const lightYellow = 'hsl(47, 100%, 63%)';
 
 // Palx colors based on #42c9ff
-const lightGray = '#bbc6ca'; // gray 4
-const mediumGray = '#92a3a8'; // gray 6
-const black = '#3a4043'; // gray 9
-const white = '#f8fafa'; // gray 0
+const lightGray = 'hsl(196, 12%, 76%)'; // gray 4
+const mediumGray = 'hsl(194, 11%, 62%)'; // gray 6
+const black = 'hsl(200, 7%, 25%)'; // gray 9
+const white = 'hsl(180, 17%, 98%)'; // gray 0
 
 const theme = {
   primary: lightBlue,
   primaryDark: darkBlue,
   secondary: lightYellow,
-  tertiary: moderateYellow,
   mediumGray: mediumGray,
   lightGray: lightGray,
   black: black,
