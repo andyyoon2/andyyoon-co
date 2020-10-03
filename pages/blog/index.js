18947e4f2f370c1f3a65bc21996593e506d8ddaa
Andy Yoon <ayoon212@gmail.com>
2020-10-03 12:28:03 -0700

    Add post numbering, improve styles

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -5,10 +5,10 @@ import { getAllPosts } from '../../lib';
 
 const BlogMain = styled.main`
   margin: 0 3rem;
-  margin-top: 12rem;
+  margin-top: 8rem;
 
   .heading {
-    margin-bottom: 6rem;
+    margin-bottom: 8rem;
   }
   h1 {
     margin: 2rem 0;
@@ -20,48 +20,50 @@ const BlogMain = styled.main`
 `;
 
 const PostWrapper = styled.div`
+  margin-bottom: 6rem;
+  position: relative;
   a {
     display: inline-block;
     width: 100%;
   }
-  .post-card {
-    border: 1px solid ${({theme}) => theme.lightGray};
-    border-radius: 2px;
-    cursor: pointer;
-    font-variation-settings: 'wght' 420;
-    padding: 0.5em 1em;
-    transition: all 0.2s ease-out;
-    
-    &:hover {
-      box-shadow: 0px 3px 5px ${({theme}) => theme.lightGray};
-      transform: translateY(-1px);
-    }
-  }
 
   .title {
     font-size: 1.5rem;
-    font-variation-settings: 'wght' 320;
+    font-variation-settings: 'wght' 620;
     margin-top: 0;
-    margin-bottom: 0.5em;
+    margin-bottom: 1em;
   }
   .description {
-    
+    color: ${({theme}) => theme.darkGray};
+    font-size: 1.125rem;
+    font-variation-settings: 'wght' 420;
   }
   .date {
     color: ${({theme}) => theme.darkGray};
-    font-size: 0.875rem;
+    font-variation-settings: 'wght' 320;
     margin-bottom: 0;
+  }
+  .number {
+    color: ${({theme}) => theme.lightGray};
+    opacity: 0.7;
+    font-size: 4rem;
+    margin: 0;
+    position: absolute;
+    top: -0.18em;
+    left: -1.75em;
+    z-index: -1;
   }
 `;
 
-const Post = ({ post }) => (
+const Post = ({ post, number }) => (
   <PostWrapper>
     <Link href={`/blog/${post.slug}`}>
       <a className="no-decoration">
-        <div>{/* <div className="post-card"> */}
+        <div>
+          <p className="number">{formatPostNumber(number)}</p>
           <h3 className="title">{post.title}</h3>
           <p className="description">{post.description}</p>
-          <p className="date">{parseDate(post.date)}</p>
+          <p className="date">{formatDate(post.date)}</p>
         </div>
       </a>
     </Link>
@@ -75,21 +77,31 @@ const Blog = ({ posts }) => (
         <h1>Blog</h1>
         <h2>Thoughts about life and stuff</h2>
       </div>
-      {posts.map(post => (
-        <Post post={post} key={post.slug} />
+      {posts.map((post, index) => (
+        <Post post={post} number={posts.length - index} key={post.slug} />
       ))}
     </BlogMain>
   </Layout>
 );
 
-function parseDate(dateString) {
-  // Assumes dateString is in format "YYYY-MM-DD"
+// Assumes dateString is in format "YYYY-MM-DD"
+function formatDate(dateString) {
   const dateParts = dateString.split('-');
   const date = new Date();
   date.setFullYear(dateParts[0]);
   date.setMonth(parseInt(dateParts[1]) - 1); // month is 0-indexed
   date.setDate(dateParts[2]);
   return date.toDateString();
+}
+
+// Formats numbers as 2-digits
+function formatPostNumber(number) {
+  const num = parseInt(number);
+  if (num >= 10) {
+    return number;
+  } else {
+    return '0' + number;
+  }
 }
 
 export async function getStaticProps() {
