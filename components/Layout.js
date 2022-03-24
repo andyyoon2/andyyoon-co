e3b9b34f62804b0906fbb0630bfb43e5bca64b5c
Andy Yoon <ayoon212@gmail.com>
2022-03-23 23:25:05 -0700

    Add dark mode!

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,24 @@
+import { useMemo } from 'react';
 import Head from 'next/head';
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import Header from './Header';
 
 export default function Layout(props) {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
   return (
     <>
       <Head>
@@ -12,12 +27,18 @@ export default function Layout(props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Container maxWidth="sm">
-        <Header />
-        <main>
-          {props.children}
-        </main>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="sm" sx={{
+          // Ideally these should be applied to <body>, maybe update css-vars in createTheme
+          backgroundColor: 'background.default',
+          color: 'text.primary',
+        }}>
+          <Header />
+          <main>
+            {props.children}
+          </main>
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
