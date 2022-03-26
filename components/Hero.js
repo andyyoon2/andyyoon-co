f00efdd3b4cef99f4dc09ef854e1b967782070ff
Andy Yoon <ayoon212@gmail.com>
2022-03-26 16:50:19 -0700

    Add mission statement, simple links

--- components/Hero.js
+++ components/Hero.js
@@ -12,12 +12,13 @@ const Bold = ({ children }) => (
 );
 
 
-const WorkSection = ({ title, place, url, src }) => (
+const WorkLine = ({ title, place, url }) => (
   <Box>
-    <Box component="h3" sx={{
-      fontWeight: 400,
+    <Box component="p" sx={{
+      // fontWeight: 400
     }}>
-      {title} at <a href={url} target="_blank">{place}</a>
+      <Bold>{title}</Bold>
+      <Box component="a" href={url} target="_blank" sx={{ display: 'block' }}>{place}</Box>
     </Box>
   </Box>
 );
@@ -25,33 +26,41 @@ const WorkSection = ({ title, place, url, src }) => (
 export default function Hero() {
   return (
     <>
-      <Box>
-        <Box component="h1" sx={{
+      <Box sx={{ marginTop: '4rem' }}>
+        <Box component="h3" sx={{
           fontWeight: 300,
           fontSize: '1.75rem',
         }}>
           Hi, I'm <KeepTogether>Andy Yoon</KeepTogether>
         </Box>
-        <Box component="h2" sx={{
+        <Box component="h1" sx={{
           fontWeight: 350,
           fontSize: '2rem',
         }}>
           <Bold>Web developer</Bold> &amp; <Bold>musician</Bold> based in
           <KeepTogether>Los Angeles</KeepTogether>
         </Box>
+        <Box component="h2" sx={{
+          fontWeight: 300,
+          fontSize: '1.25rem',
+          fontStyle: 'italic',
+        }}>
+          My mission is to use my skills to serve others and make a tangible positive contribution to the world.
+        </Box>
 
         <section>
-          <WorkSection
+          <h3>Work</h3>
+          <WorkLine
             title="Lead Software Engineer"
             place="EnterpriseAlumni"
             url="https://enterprisealumni.com/"
           />
-          <WorkSection
+          <WorkLine
             title="iOS Developer"
             place="TapFive"
             url="https://tapfive.dev/"
           />
-          <WorkSection
+          <WorkLine
             title="Guitarist and Bandleader"
             place="No Bathroom Breaks"
             url=""
