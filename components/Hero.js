93cc9f0598af59a1f5786c128e9d4513a19c0fbf
Andy Yoon <ayoon212@gmail.com>
2022-03-26 16:30:57 -0700

    Simplify styles and wording

--- components/Hero.js
+++ components/Hero.js
@@ -1,52 +1,23 @@
 import Image from 'next/image';
 import Box from '@mui/material/Box';
 
-import nbbAlbum from '../public/images/nbb-album.jpg';
-import eaLogo from '../public/images/ea-logo.png';
-
 const KeepTogether = ({ children }) => (
   <Box component="span" sx={{ display: 'inline-block' }}>{children}</Box>
 );
 
-const Heading1 = ({ children }) => (
-  <Box component="h1" sx={{
-    fontWeight: 300,
+const Bold = ({ children }) => (
+  <Box component="strong" sx={{
+    fontWeight: 600,
   }}>{children}</Box>
 );
 
-const Heading2 = ({ children }) => (
-  <Box component="h2" sx={{
-    fontWeight: 400,
-    fontStyle: 'italic',
-  }}>{children}</Box>
-);
-
-const Section = ({ children }) => (
-  <Box component="section" sx={{
-    background: 'linear-gradient(blue, purple)',
-    height: '400px',
-  }}>
-    {children}
-  </Box>
-);
 
 const WorkSection = ({ title, place, url, src }) => (
-  <Box sx={{
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    flexWrap: { xs: 'wrap', md: 'normal' },
-  }}>
-    <Box sx={{
-      flex: { xs: '1 0 100%', sm: '1 0 50%' },
-      // maxWidth: '200px',
-    }}>
-      <Image class="company-logo" src={src} alt="" />
-    </Box>
+  <Box>
     <Box component="h3" sx={{
-      flex: '1 0 50%',
+      fontWeight: 400,
     }}>
-      <h3>{title} at <a href={url} target="_blank">{place}</a></h3>
+      {title} at <a href={url} target="_blank">{place}</a>
     </Box>
   </Box>
 );
@@ -55,30 +26,35 @@ export default function Hero() {
   return (
     <>
       <Box>
-        <Heading1>Welcome! I'm <KeepTogether>Andy Yoon</KeepTogether></Heading1>
-        <Heading2>
-          <span>A web developer and musician based in </span>
+        <Box component="h1" sx={{
+          fontWeight: 300,
+          fontSize: '1.75rem',
+        }}>
+          Hi, I'm <KeepTogether>Andy Yoon</KeepTogether>
+        </Box>
+        <Box component="h2" sx={{
+          fontWeight: 350,
+          fontSize: '2rem',
+        }}>
+          <Bold>Web developer</Bold> &amp; <Bold>musician</Bold> based in
           <KeepTogether>Los Angeles</KeepTogether>
-        </Heading2>
+        </Box>
 
         <section>
           <WorkSection
             title="Lead Software Engineer"
             place="EnterpriseAlumni"
             url="https://enterprisealumni.com/"
-            src={eaLogo}
           />
           <WorkSection
             title="iOS Developer"
             place="TapFive"
             url="https://tapfive.dev/"
-            src={nbbAlbum}
           />
           <WorkSection
             title="Guitarist and Bandleader"
             place="No Bathroom Breaks"
             url=""
-            src={nbbAlbum}
           />
         </section>
       </Box>
