61378d05824a3343438b693ad41aa677276c5d17
Andy Yoon <ayoon212@gmail.com>
2022-03-26 16:50:01 -0700

    Import header styles from v1

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,14 @@ import Box from '@mui/material/Box';
 export default function Header() {
   return (
     <header>
-      <Box sx={{ position: 'sticky', top: 0 }}>
+      <Box sx={{
+        position: 'fixed',
+        top: 0,
+        width: '100%',
+        padding: '1em 0',
+        background: 'linear-gradient(rgba(255, 255, 255, 0.85) 60%, rgba(255, 255, 255, 0.4) 80%, rgba(255, 255, 255, 0.1))',
+        padding: '1.25rem 1.25rem 2.5rem',
+      }}>
         <Link href="/"><a>Home</a></Link>
         <Link href="/blog"><a>Blog</a></Link>
         <Link href="/resume"><a>Resume</a></Link>
