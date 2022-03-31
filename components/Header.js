8e48c7f0e1b760401351463fb7d4b99f7f036801
Andy Yoon <ayoon212@gmail.com>
2022-03-30 22:16:47 -0700

    Fix header background colors for dark mode

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,14 @@
 import Link from 'next/link';
 import Box from '@mui/material/Box';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+// Based off colors from globals.css
+const lightGradient = 'linear-gradient(rgba(255, 255, 255, 0.85) 60%, rgba(255, 255, 255, 0.4) 80%, rgba(255, 255, 255, 0.1))';
+const darkGradient = 'linear-gradient(hsla(256, 0%, 8%, 0.85) 60%, hsla(256, 0%, 8%, 0.4) 80%, hsla(256, 0%, 8%, 0.1))';
 
 export default function Header() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
   return (
     <header>
       <Box sx={{
@@ -9,7 +16,7 @@ export default function Header() {
         top: 0,
         width: '100%',
         padding: '1em 0',
-        background: 'linear-gradient(rgba(255, 255, 255, 0.85) 60%, rgba(255, 255, 255, 0.4) 80%, rgba(255, 255, 255, 0.1))',
+        background: prefersDarkMode ? darkGradient : lightGradient,
         padding: '1.25rem 1.25rem 2.5rem',
       }}>
         <Link href="/"><a>Home</a></Link>
