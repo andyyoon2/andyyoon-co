13ea4fee46d1692cf08ac266f639b2252caee79e
Andy Yoon <insoo.yoon@proton.me>
2023-03-02 14:36:07 -0800

    Add alternate hero with toggle for testing

--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,36 @@
+import { useState } from 'react'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import { KeepTogether } from './shared'
 
 export default function Hero () {
+  const [isSerif, setIsSerif] = useState(false)
+  // TODO 2023-03-01: This doesn't work, component ends up all lowercase. Same with <Box component="">
+  // const HeroComponent = isSerif ? 'HeroSerif' : 'HeroSansSerif';
+
+  const HeroContent = () => (
+    <>
+      <Box component="span" sx={{ display: { xs: 'block', sm: 'inline-block' } }}>
+        Calm &amp; Compassionate
+      </Box>
+      <Box component="span" sx={{ display: { xs: 'none', sm: 'inline-block' } }}>
+        &nbsp;
+      </Box>
+      <KeepTogether>Frontend Engineer</KeepTogether>
+    </>
+  )
+
   return (
     <Box
-      component='section' sx={{
-        textAlign: 'center'
-      }}
+      component='section'
+      sx={{ textAlign: 'center' }}
+      onClick={() => {setIsSerif(!isSerif)}}
     >
+      {isSerif
+        ? <HeroSerif><HeroContent /></HeroSerif>
+        : <HeroSansSerif><HeroContent /></HeroSansSerif>
+      }
+
       <Box
         component='h1'
         className='serif'
@@ -31,31 +53,19 @@ export default function Hero () {
         <KeepTogether>Frontend Engineer</KeepTogether>
       </Box>
 
-      <Box
-        component='h2' sx={{
-          fontSize: {
-            xs: '1rem',
-            sm: '1.25rem',
-            md: '1.75rem'
-          },
-          fontWeight: 'normal',
-          // margin: '0 auto',
-          marginTop: 0,
-          marginBottom: '3rem',
-          // maxWidth: '500px',
-          padding: '0 1em',
-          textAlign: 'center'
-        }}
-      >
-        {/* <Block sx={{ marginBottom: '1em' }}>
-          Hi, I&apos;m Andy. I have over 7 years of experience leading teams,
-          proactively solving problems, and creating responsive apps.
-        </Block>
-        <Block>
-          With my high-quality approach and keen eye for detail, I&apos;m ready
-          to help your team improve processes and amplify your global impact.
-        </Block> */}
-        With a high-quality approach and keen eye for detail
+      <Box component="h2" sx={{
+        fontSize: {
+          xs: '1rem',
+          sm: '1.25rem',
+          md: '1.75rem',
+        },
+        fontWeight: 'normal',
+        marginTop: 0,
+        marginBottom: '3rem',
+        padding: '0 1em',
+        textAlign: 'center',
+      }}>
+        With a high-quality approach &amp; <KeepTogether>keen eye for detail</KeepTogether>
       </Box>
 
       <a href='mailto:insoo.yoon@proton.me'>
@@ -63,4 +73,46 @@ export default function Hero () {
       </a>
     </Box>
   )
+}
+
+function HeroSansSerif({ children }) {
+  return (
+    <Box
+      component="h1"
+      sx={{
+        color: 'var(--color-primary)',
+        fontWeight: 800,
+        fontSize: '2rem',
+        letterSpacing: '-0.022em',
+        marginBottom: '0.5em',
+        // fontSize: {
+        //   xs: '1.75rem',
+        //   sm: '2rem',
+        //   md: '2.625rem',
+        // },
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
+
+function HeroSerif({ children }) {
+  return (
+    <Box
+      component="h1"
+      className="serif"
+      sx={{
+        fontWeight: 'normal',
+        marginBottom: '0.5em',
+        fontSize: {
+          xs: '1.75rem',
+          sm: '2rem',
+          md: '2.625rem',
+        },
+      }}
+    >
+      {children}
+    </Box>
+  );
 }
