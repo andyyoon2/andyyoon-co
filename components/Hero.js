70050bb381a676dd0e25d5e173c0cc061b0ee178
Andy Yoon <insoo.yoon@proton.me>
2023-03-02 15:09:32 -0800

    Choose serif heading, add letter spacing

--- components/Hero.js
+++ components/Hero.js
@@ -1,35 +1,13 @@
-import { useState } from 'react'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import { KeepTogether } from './shared'
 
 export default function Hero () {
-  const [isSerif, setIsSerif] = useState(false)
-  // TODO 2023-03-01: This doesn't work, component ends up all lowercase. Same with <Box component="">
-  // const HeroComponent = isSerif ? 'HeroSerif' : 'HeroSansSerif';
-
-  const HeroContent = () => (
-    <>
-      <Box component='span' sx={{ display: { xs: 'block', sm: 'inline-block' } }}>
-        Calm &amp; Compassionate
-      </Box>
-      <Box component='span' sx={{ display: { xs: 'none', sm: 'inline-block' } }}>
-        &nbsp;
-      </Box>
-      <KeepTogether>Frontend Engineer</KeepTogether>
-    </>
-  )
-
   return (
     <Box
       component='section'
       sx={{ textAlign: 'center' }}
-      onClick={() => { setIsSerif(!isSerif) }}
     >
-      {isSerif
-        ? <HeroSerif><HeroContent /></HeroSerif>
-        : <HeroSansSerif><HeroContent /></HeroSansSerif>}
-
       <Box
         component='h1'
         className='serif'
@@ -40,7 +18,8 @@ export default function Hero () {
             xs: '1.75rem',
             sm: '2rem',
             md: '2.625rem'
-          }
+          },
+          letterSpacing: '-0.022em'
         }}
       >
         <Box component='span' sx={{ display: { xs: 'block', sm: 'inline-block' } }}>
@@ -54,12 +33,17 @@ export default function Hero () {
 
       <Box
         component='h2' sx={{
+          fontWeight: 'normal',
           fontSize: {
             xs: '1rem',
             sm: '1.25rem',
             md: '1.75rem'
           },
-          fontWeight: 'normal',
+          letterSpacing: {
+            xs: '-0.011em',
+            sm: '-0.017em',
+            md: '-0.021em'
+          },
           marginTop: 0,
           marginBottom: '3rem',
           padding: '0 1em',
@@ -72,48 +56,6 @@ export default function Hero () {
       <a href='mailto:insoo.yoon@proton.me'>
         <Button variant='contained' size='large'>Contact Me</Button>
       </a>
-    </Box>
-  )
-}
-
-function HeroSansSerif ({ children }) {
-  return (
-    <Box
-      component='h1'
-      sx={{
-        color: 'var(--color-primary)',
-        fontWeight: 800,
-        fontSize: '2rem',
-        letterSpacing: '-0.022em',
-        marginBottom: '0.5em'
-        // fontSize: {
-        //   xs: '1.75rem',
-        //   sm: '2rem',
-        //   md: '2.625rem',
-        // },
-      }}
-    >
-      {children}
-    </Box>
-  )
-}
-
-function HeroSerif ({ children }) {
-  return (
-    <Box
-      component='h1'
-      className='serif'
-      sx={{
-        fontWeight: 'normal',
-        marginBottom: '0.5em',
-        fontSize: {
-          xs: '1.75rem',
-          sm: '2rem',
-          md: '2.625rem'
-        }
-      }}
-    >
-      {children}
     </Box>
   )
 }
