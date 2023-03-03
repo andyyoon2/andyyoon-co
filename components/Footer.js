6eef6ae648bdb570c2cd8c34f8883c51f2e3d490
Andy Yoon <insoo.yoon@proton.me>
2023-03-03 15:00:28 -0800

    Finish design for simple footer

--- components/Footer.js
+++ components/Footer.js
@@ -5,16 +5,23 @@ export default function Footer () {
   return (
     <Box component='footer' sx={{ textAlign: 'center' }}>
       <Box
-        component='p'
         className='gray-text'
         sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
           fontSize: '1rem',
-          letterSpacing: '-0.011em'
+          letterSpacing: '-0.011em',
+          marginBottom: '1em'
         }}
       >
-        {/* © {new Date().getFullYear()} Andy Yoon */}
-        <Box component='span'>Designed and built by Andy Yoon</Box>
-        <Signature className='gray-text' sx={{ height: '4rem' }} />
+        <Box className='gray-text' sx={{ flex: '1 0 33%', textAlign: 'right' }}>
+          <Signature sx={{ height: '3rem' }} />
+        </Box>
+        <Box component='span' sx={{ flex: '0 0 1.5rem' }}>©</Box>
+        <Box component='span' sx={{ flex: '1 0 33%', textAlign: 'left' }}>
+          {new Date().getFullYear()}
+        </Box>
       </Box>
     </Box>
   )
