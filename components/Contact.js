7fc6dce0b23f5d2f8e9fd60e70d3b26c51008527
Andy Yoon <insoo.yoon@proton.me>
2023-03-03 16:35:37 -0800

    Style contact section

--- components/Contact.js
+++ components/Contact.js
@@ -1,5 +1,5 @@
 import Box from '@mui/material/Box'
-import { responsiveTextH2 } from './shared/styles'
+import { responsiveTextH2, responsiveTextRegular } from './shared/styles'
 import Mail from '../svg/Mail'
 import LinkedIn from '../svg/LinkedIn'
 import GitHub from '../svg/GitHub'
@@ -27,24 +27,47 @@ const links = [
 
 export default function Contact () {
   return (
-    <section>
-      <Box component='h2' sx={{ ...responsiveTextH2 }}>Contact</Box>
-      <Box>
-        {links.map(link => (
-          <Box key={link.key}>
-            {/* https://stackoverflow.com/questions/33471880/dynamic-tag-name-in-react-jsx#comment87874314_33471928 */}
-            <Box><link.icon /></Box>
-            <Box
-              component='a'
-              href={link.href}
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              {link.label}
-            </Box>
-          </Box>
-        ))}
+    <Box component='section'>
+      <Box
+        component='h2'
+        sx={{
+          ...responsiveTextH2,
+          marginBottom: '1.25em',
+          textAlign: 'center'
+        }}
+      >Contact
       </Box>
-    </section>
+      {links.map(link => (
+        <Box
+          key={link.key}
+          sx={{
+            ...responsiveTextRegular,
+            width: '12.5rem',
+            margin: '1.5rem auto',
+            whiteSpace: 'nowrap'
+          }}
+        >
+          {/* https://stackoverflow.com/questions/33471880/dynamic-tag-name-in-react-jsx#comment87874314_33471928 */}
+          <link.icon sx={{ height: '1.375em', transform: 'translateY(0.24em)' }} />
+          <Box
+            component='a'
+            href={link.href}
+            target='_blank'
+            rel='noopener noreferrer'
+            sx={{
+              display: 'inline-block',
+              justifySelf: 'start',
+              marginLeft: '1rem',
+              transform: {
+                xs: 'translateY(-2px)',
+                sm: 'translateY(-1px)'
+              }
+            }}
+          >
+            {link.label}
+          </Box>
+        </Box>
+      ))}
+    </Box>
   )
 }
