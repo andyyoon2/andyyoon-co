bbf90a16fdde9ca75f68a862db846ec7db944297
Andy Yoon <insoo.yoon@proton.me>
2023-03-03 17:03:25 -0800

    Add tests for contact

--- __tests__/home.test.js
+++ __tests__/home.test.js
@@ -19,4 +19,14 @@ describe('Home', () => {
     const link = screen.getByRole('link', { name: /view resume/i })
     expect(link).toHaveAttribute('href', '/Andy-Yoon-resume.pdf')
   })
+
+  it('should display valid contact links', () => {
+    render(<Home />)
+    const email = screen.getByRole('link', { name: /insoo\.yoon@proton\.me/i })
+    expect(email).toHaveAttribute('href', 'mailto:insoo.yoon@proton.me')
+    const linkedin = screen.getByRole('link', { name: /in\/andyyoon2/i })
+    expect(linkedin).toHaveAttribute('href', 'https://linkedin.com/in/andyyoon2')
+    const github = screen.getByRole('link', { name: /^andyyoon2/i })
+    expect(github).toHaveAttribute('href', 'https://github.com/andyyoon2')
+  })
 })
