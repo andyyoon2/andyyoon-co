54e0548e6d8ec99c1bb9b8f4878248bbd01236c6
Andy Yoon <insoo.yoon@proton.me>
2023-12-17 22:34:33 +0000

    Restructure for client and server components

---
+++ app/home.js
@@ -0,0 +1,19 @@
+'use client'
+
+import Box from '@mui/material/Box'
+import Hero from '../components/Hero'
+import Work from '../components/Work'
+import Contact from '../components/Contact'
+// import Portfolio from '../components/Portfolio'
+
+export default function Home () {
+  return (
+    // Offset for global padding in <Layout> (see _app.js)
+    <Box sx={{ marginTop: '-20px' }}>
+      <Hero />
+      <Work />
+      {/* <Portfolio /> */}
+      <Contact />
+    </Box>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,19 +1,7 @@
-'use client'
+import Home from './home'
 
-import Box from '@mui/material/Box'
-import Hero from '../components/Hero'
-import Work from '../components/Work'
-import Contact from '../components/Contact'
-// import Portfolio from '../components/Portfolio'
-
-export default function Home () {
+export default function Page () {
   return (
-    // Offset for global padding in <Layout> (see _app.js)
-    <Box sx={{ marginTop: '-20px' }}>
-      <Hero />
-      <Work />
-      {/* <Portfolio /> */}
-      <Contact />
-    </Box>
+    <Home />
   )
 }
